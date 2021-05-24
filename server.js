217611e3fa3c24d44d1b4adaf01c1830311b7eba
srq5 <stevenesammon@hotmail.com>
2021-05-24 12:30:56 -0700

    adds index pug template

--- server.js
+++ server.js
@@ -2,13 +2,21 @@
 const express = require('express');
 const path = require('path');
 
+// Initializes application
 const app = express();
+
+//Sets the view engine to use pug.
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
+
 
 //The static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-   res.send('WELCOME TO TARNATION.NET')
+   res.render('index', {
+      title: 'TARNATION'
+   });
 });
 
 const PORT = process.env.PORT || 5000
